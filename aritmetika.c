e92d469e472137170455c06853e93fb8ebec985d
mzaldirizaldi <mzaldirizaldi@gmail.com>
2022-09-12 11:45:33 +0700

    Add files via upload

---
+++ aritmetika.c
@@ -0,0 +1,26 @@
+/* Replace "dll.h" with the name of your header */
+#include "aritmetika.h"
+
+double tambah (double a, double b) {
+	return a + b;
+}
+
+double kurang (double a, double b) {
+	return a - b;
+}
+
+double kali (double a, double b) {
+	return a * b;
+}
+
+double bagi (double a, double b) {
+	return a / b;
+}
+
+int bagi_integer (int a, int b) {
+	return a / b;
+}
+
+int sisabagi (int a, int b) {
+	return a % b;
+}
---
+++ aritmetika.h
@@ -0,0 +1,11 @@
+#ifndef _ARITMETIKA_H_
+#define _ARITMETIKA_H_
+
+extern double tambah (double a, double b);
+extern double kurang (double a, double b);
+extern double kali (double a, double b);
+extern double bagi (double a, double b);
+extern int bagi_integer (int a, int b);
+extern int sisabagi (int a, int b);
+
+#endif
